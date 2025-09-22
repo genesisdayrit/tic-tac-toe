fe6fa44179b1705013c05979479e72de6d8d5b9e
genesisdayrit <genesisdayrit@gmail.com>
2025-09-22 12:09:33 -0400

    add initial tic tac toe display

---
+++ frontend/src/components/Gameboard.tsx
@@ -0,0 +1,24 @@
+
+export default function Gameboard() {
+        
+    
+  return (
+      <>
+      <h1>Tic-Tac-Toe</h1>
+      <div className="board">
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+      </div>
+      
+      
+  </>
+  )
+  
+}
